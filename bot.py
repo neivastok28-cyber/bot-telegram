e57fd5d3457c1dccf7607c791c48b6c23ff8a823
neivastok28-cyber <neivastok28@gmail.com>
2026-05-04 17:21:47 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,18 @@
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-API_TOKEN = os.getenv("API_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot aktif 🚀")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("BOT RUNNING...")
+app.run_polling()
